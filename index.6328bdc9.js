a35a313006ff9f725b0b9a1dfdab1aa9035d12af
ValeriaSarazhinska <109595524+ValeriaSarazhinska@users.noreply.github.com>
2022-11-25 13:36:53 +0000

    Deploying to gh-pages from @ ValeriaSarazhinska/team-3-ice-cream@09d500e9fe3e33add9de973c33a65c69b1ae07b6 🚀

---
+++ index.6328bdc9.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-buy-mobile-open]"),closeModalBtn:document.querySelector("[data-modal-buy-mobile-close]"),modal:document.querySelector("[data-modal-buy-now]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.6328bdc9.js.map
